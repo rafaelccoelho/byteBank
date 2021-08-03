59c69cd44a27c44ca491f87efb47ff3d8860770a
Rafael <cezariocoelho@gmail.com>
2021-08-03 17:18:05 -0300

    Implementada a classe Funcionário

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "118.732.118 - 90";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
